feba03ae96a33115e6bfc42f4af45eb0c5f17db1
alaasassii <alaasassijs@gmail.com>
2023-02-06 21:14:22 +0100

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,11 @@ import Home from './Home';
 
 function App() {
  
-  useEffect(()=>{
-  
-
-    fetch("https://100014.pythonanywhere.com/api/userinfo/", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-    session_id:"un6beoydda13mcno4kv8lsady27motnz" ,
-    id:"100098"
-  })
-  
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log("Success:", data);
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
-  },[])
+ 
   return (
     <div className="App">
         <Routes>
-          <Route path="/:session_id" element={<Home/>}/>
-            
+          <Route path="/task" element={<Home/>}/>
         </Routes>
     </div>
   )
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Link, useSearchParams } from 'react-router-dom';
 
 const Home = () => {
-          const {session_id} = useParams()     
-          const str = session_id       
-            let values = str.split('&').map(param => {
-              let [key, value] = param.split('=');
-              return { [key]: value };
-            });
-            
-            let sessionId = values.find(param => param.hasOwnProperty('session_id'))['session_id'];
-            let id = values.find(param => param.hasOwnProperty('id'))['id'];
-            
-            console.log(sessionId, id);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const id = searchParams.get('id');
+  const session_id = searchParams.get('session_id');
+  useEffect(()=>{
+    if(id){
+      fetch("https://100014.pythonanywhere.com/api/userinfo/", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+    session_id ,
+    id
+  })
+  
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log("Success:", data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+    }
+    else{
+      fetch("https://100014.pythonanywhere.com/api/userinfo/", {
+    method: "POST",
+    headers: {
+    "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+    session_id ,
+  })
+  
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log("Success:", data);
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
+    }
+  },[])
   return (
     <>
     <div>Home</div>
