a2dc27725b7db5996229ba0566c361578023186e
alaasassii <alaasassijs@gmail.com>
2023-02-07 20:02:54 +0100

    fixing  things

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,51 +1,25 @@
 import React, { useEffect } from 'react'
 import { Link, useSearchParams } from 'react-router-dom';
+import axios from "axios";
 
 const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const id = searchParams.get('id');
   const session_id = searchParams.get('session_id');
-  useEffect(()=>{
-    if(id){
-      fetch("https://100014.pythonanywhere.com/api/userinfo/", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-    session_id ,
-    id
-  })
-  
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log("Success:", data);
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
-    }
-    else{
-      fetch("https://100014.pythonanywhere.com/api/userinfo/", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-    session_id ,
-  })
-  
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log("Success:", data);
-  })
-  .catch(error => {
-    console.error("Error:", error);
-  });
-    }
-  },[])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const endpoint = id ? "https://100093.pythonanywhere.com/api/userinfo/" : "https://100014.pythonanywhere.com/api/userinfo/";
+        const body = id ? { session_id, id } : { session_id };
+        const { data } = await axios.post(endpoint, body);
+        console.log("Success:", data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
   return (
     <>
     <div>Home</div>
