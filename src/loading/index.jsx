14b2cd95c3ae7ca551a857f622ed722c3a4af0e3
alaasassii <alaasassijs@gmail.com>
2023-02-08 22:25:41 +0100

    message

---
+++ src/loading/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './index.scss'
+const index = () => {
+  return (
+    <>
+    <div className="e-albania">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" id="orbit" className='ala-svg'>
+  <circle fill="none" stroke="#fff" strokeWidth="20" cx="200" cy="200" r="260"/>
+  </svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" id="electron" className='ala-svg'>
+  <circle id="circle" fill="#EB1E51" cx="-30" cy="180" r="20" />
+  </svg>
+  <img src="https://uxlivinglab.com/wp-content/uploads/2022/12/Social-media-automation-2.png" alt="" />
+  </div>
+  </>
+  )
+}
+
+export default index
